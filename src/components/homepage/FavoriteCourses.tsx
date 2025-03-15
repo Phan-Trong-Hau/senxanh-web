861de3b4a21c660fc984d5a1250813018a870a07
PhanTrongHau <haupk2003@gmail.com>
2025-03-16 01:07:24 +0700

    feat: enhance FavoriteCourses component with improved layout

--- src/components/homepage/FavoriteCourses.tsx
+++ src/components/homepage/FavoriteCourses.tsx
@@ -6,6 +6,7 @@ import { Asset, CTA } from '@/utils/type'
 
 import Border from '../common/Border'
 import CustomCarousel from '../common/CustomCarousel'
+import Markdown from '../common/Markdown'
 
 type Props = {
   title: string;
@@ -33,23 +34,30 @@ const FavoriteCourses = ({ title, highlightTitle, courses }: Props) => {
         <div className="mt-10">
           <CustomCarousel>
             {courses.map((course, index) => (
-              <div className="!grid grid-cols-2 gap-6" key={index}>
+              <div className="!grid grid-cols-2" key={index}>
                 <div className="flex flex-col gap-6">
-                  <Image
-                    layout="fixed"
-                    width={30}
-                    height={35}
-                    objectFit="cover"
-                    src="https://senxanh-prod-media.s3.ap-southeast-1.amazonaws.com/icon_0142af1f48.png"
-                    alt={course.title}
-                    className="object-cover"
-                  />
-                  <h3 className="font-bold text-xl text-center">
-                    {course.title}
-                  </h3>
-                  <p className="text-base text-center">{course.description}</p>
+                  <div className="flex justify-center">
+                    <Image
+                      layout="fixed"
+                      width={30}
+                      height={35}
+                      objectFit="contain"
+                      src="https://senxanh-prod-media.s3.ap-southeast-1.amazonaws.com/icon_0142af1f48.png"
+                      alt={course.title}
+                      className="object-cover h-[35px]"
+                    />
+                    <h3 className="font-bold mt-4 text-3xl text-primary text-center">
+                      {course.title}
+                    </h3>
+                  </div>
+                  <div className="text-[15px] text-center bg-favorite-course">
+                    <Markdown content={course.description} className="p-5" />
+                  </div>
                   {course.cta?.href && (
-                    <Link href={course.cta.href} className="btn-primary">
+                    <Link
+                      href={course.cta.href}
+                      className="btn-primary mx-auto"
+                    >
                       {course.cta.text}
                     </Link>
                   )}
@@ -60,6 +68,9 @@ const FavoriteCourses = ({ title, highlightTitle, courses }: Props) => {
                       src={course.heroShot.url}
                       alt={course.title}
                       className="object-cover max-h-[400px] rounded-lg leading-0"
+                      preview={{
+                        maskClassName: 'rounded-lg',
+                      }}
                     />
                   </Border>
                 </div>
